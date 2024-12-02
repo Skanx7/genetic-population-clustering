2eb963a434a779380481eb4c1b80f1131b3011de
Skander Gorgi <mohamedskander.elgorgi@ensiie.eu>
2024-12-02 11:16:37 +0100

    Added download for all chromosomes, it's needed unfortunately

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
+# Define the data directory relative to the script location
 DATA_DIR="$(dirname "$0")/../data/1000G/"
 mkdir -p "$DATA_DIR"
 
-# Download the VCF file
-# Print the current working directory
-wget -P $DATA_DIR ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
+# Base URL for the 1000 Genomes Project
+BASE_URL="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502"
 
-# Download the VCF index file (TBI)
-wget -P $DATA_DIR ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz.tbi
+# Loop through chromosomes 1 to 22
+for CHR in {1..22}; do
+    echo "Downloading chromosome $CHR data..."
 
-# Download the sample panel file
-wget -P $DATA_DIR ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel
+    # Download the VCF file
+    wget -P $DATA_DIR "$BASE_URL/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
 
+    # Download the corresponding index file (TBI)
+    wget -P $DATA_DIR "$BASE_URL/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz.tbi"
+done
 
+echo "Download completed for chromosomes 1 to 22."
